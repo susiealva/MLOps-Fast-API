d95a3d36faa2f31f12807c3bfbb801028499d2aa
RodGH26 <46480925+CidCampeador2020@users.noreply.github.com>
2026-05-01 09:39:36 +0000

    Modelos

---
+++ code_models.py
@@ -0,0 +1,27 @@
+import kagglehub
+import os
+import pandas as pd
+
+# Download latest version
+path = kagglehub.dataset_download("gauravtopre/bank-customer-churn-dataset")
+print("Path to dataset files:", path)
+
+archivo = os.listdir(path)[0]
+
+# ruta completa
+ruta_completa = os.path.join(path, archivo)
+
+# leemos el CSV
+df = pd.read_csv(ruta_completa)
+
+print("¡Éxito! Dataset cargado.")
+display(df.head())
+display(df.info())
+
+# nos quedamos solo con España
+df = df.loc[['country']=='Spain']
+
+# y elimino la columna del país, porque no la necesito para las predicciones
+df.drop(columns='country', inplace=True)
+
+display(df)
